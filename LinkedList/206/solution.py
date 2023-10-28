5ee6b4cbd017be9377084fa5b9315ad28b0726ac
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-10-28 13:52:51 +0800

    feat(linkedList): complete 206 reverse linked list

--- LinkedList/206/solution.py
+++ LinkedList/206/solution.py
@@ -21,8 +21,42 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # # iterative
+        # vArray = []
+        # curr = head
+        # while curr:
+        #     vArray.append(curr.val)
+        #     curr = curr.next
+        #
+        #
+        # nN = None
+        # for v in vArray:
+        #     nN = ListNode(v, nN)
+        #
+        # # while vArray:
+        # #     nN = ListNode(vArray.pop(), nN)
+        #
+        # return nN
 
-        return head
+        # recursive approach
+        def fn(curr: Optional[ListNode], next: Optional[ListNode]):
+            nN = ListNode(curr.val, next)
+
+            if not curr.next:
+                return nN
+            else:
+                return fn(curr.next, nN)
+
+        return fn(head, None)
+
+        # def fn(curr: Optional[ListNode]):
+        #
+        #     if not curr.next:
+        #         return ListNode(curr.val)
+        #
+        #     return ListNode(curr.val, fn(curr.next))
+        #
+        # return fn(head)
 
 
 if __name__ == "__main__":
@@ -34,4 +68,4 @@ if __name__ == "__main__":
     #     5, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))
     # )
 
-    assert s.reverseList(head) == head
+    # assert s.reverseList(head) == head
