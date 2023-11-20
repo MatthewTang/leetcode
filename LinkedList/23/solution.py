cdfe47a9cf83c1884b87be73037077ed03d691fb
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-11-20 23:17:57 +0800

    feat(linkedList): update 23, nlogk solution

--- LinkedList/23/solution.py
+++ LinkedList/23/solution.py
@@ -20,6 +20,9 @@ class ListNode:
 
 
 class Solution:
+
+    # solution 1
+    # time complexity: O(nk)
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         dummy = ListNode(0)
         head = dummy
@@ -38,6 +41,43 @@ class Solution:
 
         return dummy.next
 
+    # solution 2
+    # time complexity: O(nlogk)
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged_lists.append(self.mergeLists(l1, l2))
+            lists = merged_lists
+
+        return lists[0]
+
+    def mergeLists(self, l1, l2):
+        dummy = ListNode(0)
+        head = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                head.next = ListNode(l2.val)
+                l2 = l2.next
+
+            head = head.next
+
+        if l1:
+            head.next = l1
+        if l2:
+            head.next = l2
+
+        return dummy.next
+
 
 if __name__ == "__main__":
     s = Solution()
@@ -48,5 +88,5 @@ if __name__ == "__main__":
         1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6, ListNode(7))))))
     )
 
-    print(s.mergeKLists([l0]))
+    # print(s.mergeKLists([l0]))
     print(s.mergeKLists([l1, l2, l3]))
