089becb4cc7bc9b98ef90cf040dbf6a8230878cb
Tang Siu Hin <matthew.tang@connect.hku.hk>
2024-07-28 14:51:08 +0800

    feat(39): complete

--- Backtracking/39/solution.py
+++ Backtracking/39/solution.py
@@ -1,17 +1,40 @@
 import unittest
 import heapq
 from typing import List
+from functools import reduce
+from collections import deque
 
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        return [1]
+        res = []
+
+        def dfs(i: int, cur: List[int]) -> None:
+            total = reduce(lambda x, y: x + y, cur, 0)
+
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            cur.append(candidates[i])
+            dfs(i, cur)
+            cur.pop()
+            dfs(i + 1, cur)
+
+        dfs(0, [])
+
+        return res
 
 
 class TestSolution(unittest.TestCase):
     def test1(self):
         s = Solution()
-        self.assertEqual(s.combinationSum([1], 1), [1])
+        print(s.combinationSum([2, 3, 6, 7], 7))
+        print(s.combinationSum([1], 1))
+
+        # self.assertEqual(s.combinationSum([1], 1), [1])
 
 
 if __name__ == "__main__":
