12d33f1ed268660fbf284f0b425832f88b1132c7
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-27 18:14:29 +0800

    79

--- Backtracking/79/solution.py
+++ Backtracking/79/solution.py
@@ -1,47 +1,69 @@
 import unittest
 import heapq
-from typing import List
+from typing import List, Set, Tuple
 from functools import reduce
 from collections import deque
 
 
-class Grid:
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-
-    def __repr__(self):
-        return f"{str(self.i)}, {str(self.j)}"
-
-    def up(self):
-        self.i = self.i - 1
-
-    def down(self):
-        self.i = self.i + 1
-
-    def left(self):
-        self.j = self.j - 1
-
-    def right(self):
-        self.j = self.j + 1
-
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        return
+        ROW, COL = len(board), len(board[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def dfs(grid: Tuple[int], visited: Set[Tuple[int, int]], t: int) -> bool:
+            i, j = grid
+            if i < 0 or j < 0:
+                return False
+            if i >= ROW or j >= COL:
+                return False
+            if grid in visited:
+                return False
+            target = word[t]
+            char = board[i][j]
+            if char != target:
+                return False
+            if t == len(word) - 1:
+                return True
+
+            visited.add(grid)
+            for dr, dc in directions:
+                if dfs((i + dr, j + dc), visited, t + 1):
+                    return True
+            visited.remove(grid)
+
+            return False
+
+        for i in range(ROW):
+            for j in range(COL):
+                if dfs((i, j), set(), 0):
+                    return True
+        return False
+
 
 class TestSolution(unittest.TestCase):
     def test1(self):
+        board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+        word = "ABCCED"
         s = Solution()
+        self.assertTrue(s.exist(board, word))
 
-        g = Grid(0, 0)
-        print(g)
-        g.right()
-        print(g)
+    def test2(self):
+        board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+        word = "ABCCED"
+        s = Solution()
+        self.assertTrue(s.exist(board, word))
 
-        # print([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]][0][1])
+    def test3(self):
+        board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+        word = "ABCB"
+        s = Solution()
+        self.assertFalse(s.exist(board, word))
 
-        # self.assertTrue(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"))
+    def test4(self):
+        board = [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
+        word ="ABCESEEEFS"
+        s = Solution()
+        self.assertTrue(s.exist(board, word))
 
 
 if __name__ == "__main__":
