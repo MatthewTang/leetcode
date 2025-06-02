fc848af9674dd6ce80925937fc3789b8bf613b46
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-06-02 23:35:34 +0800

    269 update

--- AdvancedGraphs/269/solution.py
+++ AdvancedGraphs/269/solution.py
@@ -1,9 +1,10 @@
 import unittest
 from typing import Dict, List, Optional, Set
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 class Solution:
+    # dfs
     def foreignDictionary(self, words: List[str]) -> str:
         def dfs(char: str):
             if char in path:
@@ -47,8 +48,43 @@ class Solution:
             return ""
 
         top.reverse()
-
         return "".join(top)
+
+    # bfs
+    def foreignDictionary(self, words: List[str]) -> str:
+        if len(words) == 1:
+            return words[0]
+
+        out_edges = defaultdict(list)
+
+        in_degrees = {c: 0 for word in words for c in word}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            j = 0
+            while j < len(w1) and j < len(w2):
+                c1, c2 = w1[j], w2[j]
+                if c1 != c2:
+                    out_edges[c1].append(c2)
+                    in_degrees[c2] += 1
+                    break
+                j += 1
+            else:
+                if j < len(w1):
+                    return ""
+
+        q = deque([c for c in in_degrees if in_degrees[c] == 0])
+
+        res = []
+        while q:
+            curr = q.popleft()
+            res.append(curr)
+            for out_edge in out_edges[curr]:
+                in_degrees[out_edge] -= 1
+                if in_degrees[out_edge] == 0:
+                    q.append(out_edge)
+
+        return "".join(res) if len(res) == len(in_degrees) else ""
 
 
 class Test(unittest.TestCase):
@@ -83,6 +119,42 @@ class Test(unittest.TestCase):
     def test5(self):
         s = Solution()
         words = ["wrtkj", "wrt"]
+        expected = ""
+        result = s.foreignDictionary(words)
+        self.assertEqual(result, expected)
+
+    def test6(self):
+        s = Solution()
+        words = [
+            "mnop",
+            "nopq",
+            "opqr",
+            "pqrs",
+            "qrst",
+            "rstu",
+            "stuv",
+            "tuvw",
+            "uvwx",
+            "vwxy",
+            "wxyz",
+            "xyz",
+            "yz",
+            "z",
+            "mnopqr",
+            "nopqrs",
+            "opqrst",
+            "pqrstu",
+            "qrstuv",
+            "rstuvw",
+            "stuvwx",
+            "tuvwxy",
+            "uvwxyz",
+            "vwxyz",
+            "wxyza",
+            "xyzab",
+            "yzabc",
+            "zabcd",
+        ]
         expected = ""
         result = s.foreignDictionary(words)
         self.assertEqual(result, expected)
